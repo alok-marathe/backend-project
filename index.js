787167f48b2d0290f207373def5db52b2c38f4d1
Alok <124490591+alok-marathe@users.noreply.github.com>
2024-07-12 13:03:07 +0000

    Add valid endpoint

--- index.js
+++ index.js
@@ -20,12 +20,24 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+  let date;
+
+  if (!req.params.date) {
+    date = new Date();
+  } else if (!isNaN(req.params.date)) {
+    date = new Date(parseInt(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+      res.json({ unix: date.getTime(), utc: date.toUTCString() });
+      console.log({ unix: date.getTime(), utc: date.toUTCString() });
+  }
 });
-
-
-
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
